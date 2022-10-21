2e9ff2d3eb5c892fa677b7b771b4dcb977d6490d
Zaini Muhaimin <zainimuhaimin79@gmail.com>
2022-10-21 13:27:21 +0700

    init

---
+++ src/test/java/idn/code/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package idn.code.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
